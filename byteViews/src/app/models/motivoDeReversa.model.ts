79d1ece490b8369b2d9c3378a1e57221e493b1e5
Krowser1809 <gabrielcaba1809@gmail.com>
2019-06-13 19:05:00 -0600

    selects anadidos

--- byteViews/src/app/models/motivoDeReversa.model.ts
+++ byteViews/src/app/models/motivoDeReversa.model.ts
@@ -1,13 +1,12 @@
 export class MotivoDeReversa {
     constructor(
-        public cobroAdicional: String,
         public code: Number,
         public codigo: Number,
         public descripcion: String,
         public description: String,
         public empresa: String,
         public errorCore: Boolean,
-        public institucion: Number,
+        public institucion: String,
         public referencia1: String,
         public referencia2: String,
         public referencia3: String,
